af01ec45d6bd5a6e65b40295f8bf59c2136f3c2a
williamxmejia <willx1995@gmail.com>
2022-10-07 15:34:34 -0500

    feat: home page

---
+++ app.js
@@ -0,0 +1,29 @@
+//Imports
+
+const express = require('express');
+// const expressLayouts = require('express-ejs-layouts')
+const db = require('./config/database');
+
+
+const port = 3000;
+
+
+const app = express();
+
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+
+
+// app.use(expressLayouts);
+app.set('views', './views/layouts');
+// app.set('layout', './layouts')
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+
+app.listen(3000, () => {
+    console.log(`listening from port: ${port}!`);
+});
