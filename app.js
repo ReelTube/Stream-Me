aae056f11b632b776324f33d4d45967be4de4faa
williamxmejia <willx1995@gmail.com>
2022-10-10 13:03:32 -0500

    made the form for videos submit, only did name and description, still have to figure out how to upload videos and store them in a directory

--- app.js
+++ app.js
@@ -3,12 +3,14 @@
 const express = require('express');
 // const expressLayouts = require('express-ejs-layouts')
 const db = require('./config/database');
+const bodyParser = require('body-parser');
 
 
 const port = 3000;
 
 
 const app = express();
+const ejs = require('ejs');
 
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'public/css'));
@@ -18,6 +20,10 @@ app.use('/css', express.static(__dirname + 'public/css'));
 app.set('views', './views/layouts');
 // app.set('layout', './layouts')
 app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
     res.render('home', {title: 'ReelTube'});
@@ -31,8 +37,20 @@ app.get('/signup', (req, res) => {
     res.render('signup');
 });
 
-app.get('/upload', (req, res) => {
+app.get('/upload', (req, res, next) => {
     res.render('video-upload-form');
+});
+
+app.post('/upload',(req, res, next) => {
+    let title = req.body.title;
+    let description = req.body.description;
+    // let upload = req.body.uploaded;
+    let sql = `INSERT INTO videos(name,description, user_id) VALUES("${title}", "${description}", 1)`;
+
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        res.redirect('/');
+    });
 });
 
 
--- seeder.sql
+++ seeder.sql
@@ -1,3 +1,4 @@
+-- Active: 1665410732888@@127.0.0.1@3306@ReelTube_db
 USE ReelTube_db;
 
 CREATE TABLE IF NOT EXISTS users (
@@ -11,7 +12,8 @@ CREATE TABLE IF NOT EXISTS videos (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     description TEXT NOT NULL,
-    uploaded DATETIME NOT NULL,
-    PRIMARY KEY(id);
+    user_id INT UNSIGNED NOT NULL,
+    -- uploaded DATETIME NOT NULL,
+    PRIMARY KEY(id),
     FOREIGN KEY(user_id) REFERENCES users (id)
 );
