93b53d93b490b9321cea00b8692236d041322694
williamxmejia <willx1995@gmail.com>
2023-01-11 11:28:28 -0600

    created the single video page, still need to fix somethings but will do it once I add my videos and link them to my db, also made some changes to the footer

--- client/pages/watch/videopage.js
+++ client/pages/watch/videopage.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
+import dynamic from 'next/dynamic';
+const ReactPlayer = dynamic(() => import('react-player/lazy'), { ssr: false });
 
 export default function VideoPage() {
   return (
@@ -7,10 +9,20 @@ export default function VideoPage() {
       <Head>
         <title>Watching</title>
       </Head>
-      <h1>Money Ball</h1>
-      <h2>
-        <Link href="/">← Back to home</Link>
-      </h2>
+      <h1 className="single-view-center">Money Ball</h1>
+      <div className="center-player">
+        <ReactPlayer url="https://www.youtube.com/watch?v=JjX9L9Qtjvo" />
+      </div>
+      <ul>
+        <div className="contain-single">
+          <div>
+            <li>Title:</li>
+            <li>Director:</li>
+            <li>Actors:</li>
+            <li>Year:</li>
+          </div>
+        </div>
+      </ul>
     </>
   );
 }
